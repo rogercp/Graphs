5a57cdaeeb641e0206180d42c375e51b2535d9c1
Rogelio <rogercp1234@yahoo.com>
2019-10-08 21:47:07 -0700

    created new method for retrieving ancestors and imported stack or que in case i need it

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,14 @@
+from util import Stack,Queue 
+
+def retrieve_ancestors(ancestors,node):
+    ancestor_nodes = []
+    for edge in ancestors:
+        if edge[1] is node:
+            ancestor_nodes.append(edge[0])
+    return ancestor_nodes
+
+
+
 
 def earliest_ancestor(ancestors, starting_node):
     pass
